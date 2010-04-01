c306b36bf2af0552c879503aa47c455f1ab56f5d
Tres Seaver <tseaver@palladion.com>
2010-04-01 17:05:07 +0000

    Conform setup metadata, copyright headers to repository policy.

---
+++ __init__.py
@@ -0,0 +1,44 @@
+##############################################################################
+#
+# Copyright (c) 2002 Zope Foundation and Contributors.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+"""General exceptions that wish they were standard exceptions
+
+These exceptions are so general purpose that they don't belong in Zope
+application-specific packages.
+
+$Id$
+"""
+
+from unauthorized import Unauthorized
+
+from zope.interface import implements
+from zope.interface.common.interfaces import IException
+from zope.publisher.interfaces import INotFound
+from zope.security.interfaces import IForbidden
+
+class BadRequest(Exception):
+    implements(IException)
+
+class InternalError(Exception):
+    implements(IException)
+
+class NotFound(Exception):
+    implements(INotFound)
+
+class Forbidden(Exception):
+    implements(IForbidden)
+
+class MethodNotAllowed(Exception):
+    pass
+
+class Redirect(Exception):
+    pass
