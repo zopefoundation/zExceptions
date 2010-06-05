69edb4700a7242b8af2cab3273c8d2bdd726f952
Hanno Schlichting <hanno@hannosch.eu>
2010-06-05 15:53:55 +0000

    Get the code in

---
+++ README.txt
@@ -0,0 +1,5 @@
+Overview
+========
+
+zExceptions contains common exceptions and helper functions related to
+exceptions as used in Zope 2.
---
+++ setup.py
@@ -0,0 +1,37 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import setup, find_packages
+
+setup(name='zExceptions',
+      version = '2.13.0dev',
+      url='http://pypi.python.org/pypi/zExceptions',
+      license='ZPL 2.1',
+      description="zExceptions contains common exceptions used in Zope2.",
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description=open('README.txt').read() + '\n' +
+                       open('CHANGES.txt').read(),
+
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      install_requires=[
+        'setuptools',
+        'zope.interface',
+        'zope.publisher',
+        'zope.security',
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      )
