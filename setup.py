67b167e6ddccbb45a65a9bdfbd2aa8c9a11983e2
Michael Howitz <icemac@gmx.net>
2023-06-28 08:04:46 +0200

    Preparing release 5.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='zExceptions',
-    version='5.0.dev0',
+    version='5.0',
     url='https://github.com/zopefoundation/zExceptions',
     license='ZPL 2.1',
     description="zExceptions contains common exceptions used in Zope.",
